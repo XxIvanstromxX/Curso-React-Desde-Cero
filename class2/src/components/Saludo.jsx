7322ad7c8daf040554842bdf283587f575ff92e6
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-11-05 22:36:34 -0600

    Agregar componente Saludo para mostrar un saludo personalizado con nombre y edad

---
+++ class2/src/components/Saludo.jsx
@@ -0,0 +1,10 @@
+const Saludo = ({name, edad}) => {
+    return (
+        <div>
+            <h1>Hola, {name}</h1>
+            <p>Tu edad es de: {edad}</p>
+        </div>
+    )
+}
+
+export default Saludo;
