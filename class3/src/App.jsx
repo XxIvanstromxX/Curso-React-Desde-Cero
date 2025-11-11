f564600a228e0dffddd517020600beec50229541
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-11-10 22:03:13 -0600

    Agregar componente App con botones y entrada para la Clase 3 del curso de React

---
+++ class3/src/App.jsx
@@ -0,0 +1,16 @@
+import Boton from "./components/boton"
+import Entrada from "./components/input"
+import ConversorUnidades from "./components/conversorUnidades"
+
+const App = () => {
+
+  return (
+    <>
+      <Boton/>
+      <Entrada/>
+      <ConversorUnidades></ConversorUnidades>
+    </>
+  )
+}
+
+export default App
