94f22c14509f64b7671e3051f3d4dc153eaac4a6
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-11-12 23:02:00 -0600

    Agregar componente Division con estructura inicial y contenido

---
+++ class4/src/components/div.jsx
@@ -0,0 +1,10 @@
+const Division = () => {
+    return (
+        <div>
+            <h2>Componente de División</h2>
+            <p>Hola desde componente</p>
+        </div>
+    );
+}
+
+export default Division;
