4bcacdc73d11ccd9d1cdc2b9d9d66ce4df86019a
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-11-17 22:59:33 -0600

    Agregar componente App con importaciones y renderizado del componente Usuarios

---
+++ class5/src/App.jsx
@@ -0,0 +1,13 @@
+import { HookEffect } from "./components/hookEffect"
+import DataFetch from "./components/dataFetch"
+import Usuarios from "./components/usuarios"
+
+const App = () => {
+  return (
+    <>
+      <Usuarios />
+    </>
+  )
+}
+
+export default App
